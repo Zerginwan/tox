c57f1e5c123615f944783c89545f30dde188477b
ibarkhatov <igorlpu@gmail.com>
2021-10-12 23:17:35 +0300

    add auth template

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const authRouter = require('./authRouter');
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+app.use('/auth', authRouter);
+
 const start = () => {
   try {
-      app.listen( PORT, () => {
+    app.listen(PORT, () => {
       console.log(`TOX server has been started on ${PORT}`)
     })
   } catch (e) {
